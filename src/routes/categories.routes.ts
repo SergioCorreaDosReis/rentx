2f3f898c7b18286e621d35904ba6ec361ef87454
Sergio Reis <sergioc_reis@hotmail.com>
2021-05-13 07:21:39 -0300

    first commit with post and get categories running well

---
+++ src/routes/categories.routes.ts
@@ -0,0 +1,24 @@
+import { Router } from "express";
+
+import { CategoryRepository } from "../repositories/CategoriesRepository";
+import { CreateCategoryService } from "../services/CreateCategoryService";
+
+const categoriesRoutes = Router();
+const categoryRepository = new CategoryRepository();
+
+categoriesRoutes.post("/", (request, response) => {
+    const { name, description } = request.body;
+
+    const createCategoryService = new CreateCategoryService(categoryRepository);
+
+    createCategoryService.execute({ name, description });
+
+    return response.status(201).send();
+});
+
+categoriesRoutes.get("/", (request, response) => {
+    const listAllCategories = categoryRepository.list();
+    return response.status(201).json(listAllCategories);
+});
+
+export { categoriesRoutes };
