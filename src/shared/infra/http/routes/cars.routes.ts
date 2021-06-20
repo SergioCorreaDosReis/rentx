beabcf61ae77a0987a9aba53351f26cfebb65f0f
Sergio Reis <sergioc_reis@hotmail.com>
2021-06-20 19:18:08 -0300

    refactor all routes except user to include admin authentication middleware

--- src/shared/infra/http/routes/cars.routes.ts
+++ src/shared/infra/http/routes/cars.routes.ts
@@ -2,10 +2,18 @@ import { Router } from "express";
 
 import { CreateCarController } from "@modules/cars/useCases/createCar/CreateCarController";
 
+import { ensureAdmin } from "../middlewares/ensureAdmin";
+import { ensureAuthenticated } from "../middlewares/ensureAuthenticated";
+
 const carsRoutes = Router();
 
 const createCarController = new CreateCarController();
 
-carsRoutes.post("/", createCarController.handle); // esta somente como "/" por que a referencia é feita no index.ts
+carsRoutes.post(
+	"/", // esta somente como "/" por que a referencia é feita no index.ts
+	ensureAuthenticated,
+	ensureAdmin,
+	createCarController.handle
+);
 
 export { carsRoutes };
