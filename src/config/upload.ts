5f491d1fc825819d6fdc4ba2abd23f670f96f320
Sergio Reis <sergioc_reis@hotmail.com>
2021-06-30 19:29:10 -0300

    include upload of images of car

--- src/config/upload.ts
+++ src/config/upload.ts
@@ -3,19 +3,19 @@ import multer from "multer";
 import { resolve } from "path";
 
 export default {
-    upload(folder: string) {
-        return {
-            storage: multer.diskStorage({
-                destination: resolve(__dirname, "..", "..", folder),
+	upload(folder: string) {
+		return {
+			storage: multer.diskStorage({
+				destination: resolve(__dirname, "..", "..", folder),
 
-                filename: (request, file, callback) => {
-                    const filehash = crypto.randomBytes(16).toString("hex");
+				filename: (request, file, callback) => {
+					const filehash = crypto.randomBytes(16).toString("hex");
 
-                    const filename = `${filehash}-${file.originalname}`;
+					const filename = `${filehash}-${file.originalname}`;
 
-                    return callback(null, filename);
-                },
-            }),
-        };
-    },
+					return callback(null, filename);
+				},
+			}),
+		};
+	},
 };
--- src/shared/infra/http/routes/cars.routes.ts
+++ src/shared/infra/http/routes/cars.routes.ts
@@ -1,8 +1,11 @@
 import { Router } from "express";
+import multer from "multer";
 
+import uploadConfig from "@config/upload";
 import { CreateCarController } from "@modules/cars/useCases/createCar/CreateCarController";
 import { CreateCarSpecificationController } from "@modules/cars/useCases/createCarSpecification/CreateCarSpecificationController";
 import { ListAvailableCarsController } from "@modules/cars/useCases/listAvailableCars/ListAvailableCarsController";
+import { UploadCarImageController } from "@modules/cars/useCases/uploadCarImage/UploadCarImageController";
 
 import { ensureAdmin } from "../middlewares/ensureAdmin";
 import { ensureAuthenticated } from "../middlewares/ensureAuthenticated";
@@ -12,6 +15,9 @@ const carsRoutes = Router();
 const createCarController = new CreateCarController();
 const listAvailableCarsController = new ListAvailableCarsController();
 const createCarSpecificationController = new CreateCarSpecificationController();
+const uploadCarsImageController = new UploadCarImageController();
+
+const upload = multer(uploadConfig.upload("./tmp/cars"));
 
 carsRoutes.post(
 	"/", // esta somente como "/" por que a referencia é feita no index.ts
@@ -27,6 +33,14 @@ carsRoutes.post(
 	ensureAuthenticated,
 	ensureAdmin,
 	createCarSpecificationController.handle
+);
+
+carsRoutes.post(
+	"/images/:id",
+	ensureAuthenticated,
+	ensureAdmin,
+	upload.array("images"), // usar o mesmo nome que utilizou para variavel request.files as IFiles[]
+	uploadCarsImageController.handle
 );
 
 export { carsRoutes };
